1e6681589bfcc84c951b4df6be2972bf75c33f80
ThomasGrare <thomasgrare64@gmail.com>
2024-11-27 14:26:45 +0100

    PR feedback

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,10 +2,6 @@ function Register() {
     return (
       <>
         <h1>Register</h1>
-        <p>
-          Passionné de développement web, je crée des applications web performantes et esthétiques. 
-          Mon objectif est de fournir des expériences utilisateur exceptionnelles grâce à un code propre et optimisé.            
-        </p>
       </>
     );
   };
